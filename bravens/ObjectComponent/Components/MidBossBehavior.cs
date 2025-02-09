4e24bb64db7a6cd2f902ae435fb81e17da7728c9
dantecofanowsu <144556061+dantecofanowsu@users.noreply.github.com>
2025-02-09 01:53:24 -0800

    Created logic for MidBoss Enemy

---
+++ bravens/ObjectComponent/Components/MidBossBehavior.cs
@@ -0,0 +1,85 @@
+﻿using bravens.ObjectComponent.Objects;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bravens.ObjectComponent.Components
+{
+    public class MidBossBehavior : Component
+    {
+        private readonly Transform transform;
+
+        private readonly Sprite sprite;
+        //private readonly EnemyBGun gun;
+
+        private float speed = 500.0f;
+        private int xDirection = 1;
+
+        private double timeBetweenDirectionSwitches = 1.0;
+        private double accumulatedTime = 0.0;
+
+        public MidBossBehavior(GameObject parent) : base(parent, nameof(EnemyABehaviour))
+        {
+            Random random = new Random();
+            GraphicsDeviceManager graphics = GetGameObject().Core.GraphicsDeviceManager;
+            transform = parent.GetComponent<Transform>();
+            sprite = parent.GetComponent<Sprite>();
+            //gun = parent.GetComponent<EnemyBGun>();
+
+            transform.SetPositionX(random.Next(sprite.SpriteTexture.Width / 2, graphics.PreferredBackBufferWidth - sprite.SpriteTexture.Width / 2));
+        }
+
+        public override void Update(GameTime deltaTime)
+        {
+            Vector2 movement = Vector2.Zero;
+            movement.X = xDirection;
+
+            transform.Translate(movement * speed * (float)deltaTime.ElapsedGameTime.TotalSeconds);
+
+            accumulatedTime += deltaTime.ElapsedGameTime.TotalSeconds;
+
+            SwitchDirections(deltaTime);
+            KeepFromTopOfScreen();
+        }
+
+        private void SwitchDirections(GameTime deltaTime)
+        {
+            GraphicsDeviceManager graphics = GetGameObject().Core.GraphicsDeviceManager;
+            if (accumulatedTime + deltaTime.ElapsedGameTime.TotalSeconds >= timeBetweenDirectionSwitches)
+            {
+                Random random = new Random();
+                int newDirection = random.Next(0, 2);
+                if(newDirection == 0)
+                {
+                    xDirection = -1;
+                }
+                else
+                {
+                    xDirection = 1;
+                }
+
+                accumulatedTime = 0f;
+            }
+
+            if (transform.Position.X > graphics.PreferredBackBufferWidth - sprite.SpriteTexture.Width / 2)
+            {
+                xDirection = -1;
+            }
+            else if (transform.Position.X < sprite.SpriteTexture.Width / 2)
+            {
+                xDirection = 1;
+            }
+        }
+
+        private void KeepFromTopOfScreen()
+        {
+            if (transform.Position.Y < sprite.SpriteTexture.Height / 2)
+            {
+                transform.SetPositionY(sprite.SpriteTexture.Height / 2);
+            }
+        }
+    }
+}
---
+++ bravens/ObjectComponent/Components/MidBossGun.cs
@@ -0,0 +1,66 @@
+﻿using bravens.Managers;
+using bravens.ObjectComponent.Objects;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bravens.ObjectComponent.Components
+{
+    public class MidBossGun : Component
+    {
+        private GameObjectManager GameObjectManager { get; }
+
+        private double timeBetweenProjectileInSeconds = 0.8;
+
+        private int projectileCount = 0;
+        private double accumulatedTime = 0.0;
+
+        private int projectileSpreadDistance = 50;
+
+        public MidBossGun(GameObject parent) : base(parent, nameof(EnemyAGun))
+        {
+            GameObjectManager = parent.Core.gameObjectManager;
+        }
+
+        public override void Update(GameTime deltaTime)
+        {
+            if (accumulatedTime + deltaTime.ElapsedGameTime.TotalSeconds >= timeBetweenProjectileInSeconds)
+            {
+                CreateAndFireProjectile();
+
+                accumulatedTime = 0f;
+            }
+            else
+            {
+                accumulatedTime += deltaTime.ElapsedGameTime.TotalSeconds;
+            }
+        }
+
+        private void CreateAndFireProjectile()
+        {
+            Vector2 position = GetGameObject().GetComponent<Transform>().Position;
+            GameObject projectile1 = GameObjectManager.Create($"MidBossProjectile{projectileCount}", GetGameObject(), "enemyAProjectile");
+            projectile1.AddComponent<MidBossProjectile>();
+            Transform transform1 = projectile1.GetComponent<Transform>();
+            transform1.Translate(position);
+            projectileCount++;
+
+            GameObject projectile2 = GameObjectManager.Create($"MidBossProjectile{projectileCount}", GetGameObject(), "enemyAProjectile");
+            projectile2.AddComponent<MidBossProjectile>();
+            Transform transform2 = projectile2.GetComponent<Transform>();
+            position.X = position.X + projectileSpreadDistance;
+            transform2.Translate(position);
+            projectileCount++;
+
+            GameObject projectile3 = GameObjectManager.Create($"MidBossProjectile{projectileCount}", GetGameObject(), "enemyAProjectile");
+            projectile3.AddComponent<MidBossProjectile>();
+            Transform transform3 = projectile3.GetComponent<Transform>();
+            position.X = position.X - projectileSpreadDistance * 2;
+            transform3.Translate(position);
+            projectileCount++;
+        }
+    }
+}
