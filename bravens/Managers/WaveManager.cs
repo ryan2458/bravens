de6572bc8266b465a28b1666e6fedb11530aa8df
aidangaetano11 <aidan.gaetano@wsu.edu>
2025-02-12 14:03:29 -0800

    Created BaseObject of WaveManager

---
+++ bravens/Managers/WaveManager.cs
@@ -0,0 +1,23 @@
+﻿using bravens.ObjectComponent;
+using Microsoft.Xna.Framework;
+
+namespace bravens.Managers
+{
+    public class WaveManager : BaseObject
+    {
+        private GameCore gameCore;
+        private GameObjectManager gameObjectManager;
+
+        public WaveManager(GameCore core) : base(nameof(WaveManager))
+        {
+            gameCore = core;
+            gameObjectManager = core.GameObjectManager;
+        }
+
+        public override void Initialize() { }
+        public override void Load() { }
+        public override void Unload() { }
+        public override void Update(GameTime deltaTime) { }
+        public override void Draw() { }
+    }
+}
