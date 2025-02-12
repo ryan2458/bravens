2cf6ec609a3966d1f0679f2d4965720babccb7fe
aidangaetano11 <aidan.gaetano@wsu.edu>
2025-02-12 15:01:25 -0800

    Created spawn times for each enemy type

--- bravens/Managers/WaveManager.cs
+++ bravens/Managers/WaveManager.cs
@@ -1,5 +1,4 @@
 ﻿using bravens.ObjectComponent;
-using bravens.ObjectComponent.Objects;
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
@@ -11,10 +10,20 @@ namespace bravens.Managers
         private GameCore gameCore;
         private GameObjectManager gameObjectManager;
         private TimeSpan globalTimer;
+        private int globalTimerInSeconds;
 
-        private List<int> _enemyASpawnTimes = [5, 10, 20];
+        private List<int> _enemyASpawnTimes = [5,7,10,15,18];
         private int _enemyASpawnIndex = 0;
-        private int _previousTimeInSeconds = 0;
+
+        private List<int> _enemyBSpawnTimes = [13, 19];
+        private int _enemyBSpawnIndex = 0;
+
+
+        private List<int> _bossSpawnTimes = [25];
+        private int _bossSpawnIndex = 0;
+
+
+        private int _timeLastCheckedInSeconds = 0;
 
         public WaveManager(GameCore core) : base(nameof(WaveManager))
         {
@@ -28,26 +37,49 @@ namespace bravens.Managers
         public override void Update(GameTime deltaTime) 
         {
             globalTimer += deltaTime.ElapsedGameTime;
+            globalTimerInSeconds = (int)globalTimer.TotalSeconds;
             
-            if (_previousTimeInSeconds != (int)globalTimer.TotalSeconds) 
+            // This is to prevent spawning multiple enemies of the same type at once
+            if (_timeLastCheckedInSeconds != globalTimerInSeconds) 
             {
-                CheckEnemySpawn(_enemyASpawnTimes, ref _enemyASpawnIndex);
-                Console.WriteLine((int)globalTimer.TotalSeconds);
-                Console.WriteLine(($"Current Spawn Index: {_enemyASpawnIndex}"));
-                _previousTimeInSeconds = (int)globalTimer.TotalSeconds;
+                Console.WriteLine($"Current Spawn Timer: {globalTimerInSeconds}");
+
+                CheckEnemySpawn(_enemyASpawnTimes, ref _enemyASpawnIndex, "EnemyA");
+                CheckEnemySpawn(_enemyBSpawnTimes, ref _enemyBSpawnIndex, "EnemyB");
+                CheckEnemySpawn(_bossSpawnTimes, ref _bossSpawnIndex, "Boss");
+
+
+                _timeLastCheckedInSeconds = globalTimerInSeconds;
             }
 
             
         }
         public override void Draw() { }
 
-        private void CheckEnemySpawn(List<int> spawnTimes, ref int currentSpawnIndex) 
+        private void CheckEnemySpawn(List<int> spawnTimes, ref int currentSpawnIndex, string enemyType) 
         {
             if (currentSpawnIndex >= spawnTimes.Count) return;
 
-            if ((int)globalTimer.TotalSeconds == spawnTimes[currentSpawnIndex]) 
+            if (globalTimerInSeconds == spawnTimes[currentSpawnIndex]) 
             {
-                gameCore.CreateEnemyTypeA();
+                switch (enemyType) 
+                {
+                    case "EnemyA":
+                        gameCore.CreateEnemyTypeA();
+                        break;
+                    case "EnemyB":
+                        gameCore.CreateEnemyTypeB();
+                        break;
+                    case "Boss":
+                        gameCore.CreateBoss();
+                        break;
+                    default:
+                        Console.WriteLine("Could not find enemy type. Spawning EnemyA...");
+                        gameCore.CreateEnemyTypeA();
+                        break;
+                }
+
+                
 
                 currentSpawnIndex++;
             }          
