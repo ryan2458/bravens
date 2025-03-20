9ef9ce32e6ebc66508934929d055a6ce66264582
Ryan Aloof <r-aloof@outlook.com>
2025-03-19 21:59:02 -0700

    Added collision to boss projectiles

--- bravens/ObjectComponent/Interfaces/ICollisionObserver.cs
+++ bravens/ObjectComponent/Interfaces/ICollisionObserver.cs
@@ -9,6 +9,12 @@ namespace bravens.ObjectComponent.Interfaces
 {
     internal interface ICollisionObserver
     {
+        /// <summary>
+        /// Collision event handler raised when a collision occurs.
+        /// </summary>
+        /// <param name="collider">
+        /// The collider that has been collided against.
+        /// </param>
         void OnCollisionEnter(Collider collider);
     }
 }
